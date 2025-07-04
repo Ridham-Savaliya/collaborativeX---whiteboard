ce9e7f515b085f04b1984a44ac074a0459c46e17
Ridham-Savaliya <savaliyaridham46@.email.com>
2025-07-04 11:06:11 +0530

    Bug: Resovled production bugs!

--- collaborativex/app/api/user/profile/settings/route.ts
+++ collaborativex/app/api/user/profile/settings/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/app/api/_lib/db";
-import User from "@/models/User";
+import User from "@/app/api/models/User";
 import { authenticate } from "@/app/api/_lib/authMiddleware";
 
 
