7efd159c700c8140af6547194da6d8bd86af286e
Ridham-Savaliya <savaliyaridham46@.email.com>
2025-08-04 12:15:15 +0530

    .

--- collaborativex/app/utills/resendClient.ts
+++ collaborativex/app/utills/resendClient.ts
@@ -3,6 +3,6 @@ import { configDotenv } from 'dotenv';
 import { Resend } from 'resend';
 
 
-console.log(process.env.RESEND_API_KEY!);
+// console.log(process.env.RESEND_API_KEY!);
 
 export const resend = new Resend('re_GPPuSMNM_GasqS8PMcVb4QLMx8ez3HY5n');
